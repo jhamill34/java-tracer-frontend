1372af055890e072869b1975001c655a4b404a7b
Josh Hamill <joshrasmussen34@gmail.com>
2022-09-09 17:59:27 -0400

    feat: add apollo client

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,4 @@
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client"
 import React from "react"
 import { createRoot } from "react-dom/client"
 
@@ -5,9 +6,18 @@ import { App } from "./components/app/App"
 
 const container = document.getElementById("root")
 
+const client = new ApolloClient({
+    uri: `http://localhost:3000`,
+    cache: new InMemoryCache(),
+})
+
 if (container !== null) {
     const root = createRoot(container)
-    root.render(<App />)
+    root.render(
+        <ApolloProvider client={client}>
+            <App />
+        </ApolloProvider>,
+    )
 } else {
     console.error("Something went wrong rendering this app.")
 }
